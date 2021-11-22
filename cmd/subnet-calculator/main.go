c9d8e1c78fb248342387ccace1a0977cc68afa46
Olivér <oliver.mrakovics@gmail.com>
2021-11-22 17:30:46 +0100

    UI Update

--- cmd/subnet-calculator/main.go
+++ cmd/subnet-calculator/main.go
@@ -20,47 +20,10 @@ func main() {
 	title()
 	reader := bufio.NewReader(os.Stdin)
 
-	message("$", "Specify the IP address you want to calculate:")
 	for {
-		prompt("ip")
-		input, _ := reader.ReadString('\n')
-		input = strings.TrimSuffix(input, "\n")
-		fmt.Print("\u001b[0m")
-
-		if input == "" || input == " " {
-			continue
-		}
-
-		if ips.IsValidIp(input) {
-			ip = input
-			break
-		}
-		error("Please specify a valid IP address!")
-	}
-
-	message("$", "Which one do you want to specify?")
-	fmt.Println(" \u001b[32;1m~ [1] \u001b[0mNetwork mask (E.g 255.255.255.0)")
-	fmt.Println(" \u001b[32;1m~ [2] \u001b[0mNumber of bits identifying the network (E.g 24)")
-	for {
-		prompt("choose")
-		input, _ := reader.ReadString('\n')
-		input = strings.TrimSuffix(input, "\n")
-		fmt.Print("\u001b[0m")
-
-		if input == "" || input == " " {
-			continue
-		}
-
-		if input == "1" || input == "2" {
-			choice = input
-			break
-		}
-		error("Please specify 1 or 2!")
-	}
-
-	if choice == "1" {
+		message("$", "Specify the IP address you want to calculate:")
 		for {
-			prompt("mask")
+			prompt("ip")
 			input, _ := reader.ReadString('\n')
 			input = strings.TrimSuffix(input, "\n")
 			fmt.Print("\u001b[0m")
@@ -70,14 +33,17 @@ func main() {
 			}
 
 			if ips.IsValidIp(input) {
-				mask = input
+				ip = input
 				break
 			}
-			error("Please specify a valid network mask!")
+			error("Please specify a valid IP address!")
 		}
-	} else {
+
+		message("$", "Which one do you want to specify?")
+		fmt.Println(" \u001b[32;1m~ [1] \u001b[0mNetwork mask (E.g 255.255.255.0)")
+		fmt.Println(" \u001b[32;1m~ [2] \u001b[0mNumber of bits identifying the network (E.g 24)")
 		for {
-			prompt("bits")
+			prompt("choose")
 			input, _ := reader.ReadString('\n')
 			input = strings.TrimSuffix(input, "\n")
 			fmt.Print("\u001b[0m")
@@ -86,22 +52,68 @@ func main() {
 				continue
 			}
 
-			if ips.IsValidMask(input) {
-				bits = input
+			if input == "1" || input == "2" {
+				choice = input
 				break
 			}
-			error("Please specify a valid argument!")
+			error("Please specify 1 or 2!")
+		}
+
+		if choice == "1" {
+			for {
+				prompt("mask")
+				input, _ := reader.ReadString('\n')
+				input = strings.TrimSuffix(input, "\n")
+				fmt.Print("\u001b[0m")
+
+				if input == "" || input == " " {
+					continue
+				}
+
+				if ips.IsValidIp(input) {
+					mask = input
+					break
+				}
+				error("Please specify a valid network mask!")
+			}
+		} else {
+			for {
+				prompt("bits")
+				input, _ := reader.ReadString('\n')
+				input = strings.TrimSuffix(input, "\n")
+				fmt.Print("\u001b[0m")
+
+				if input == "" || input == " " {
+					continue
+				}
+
+				if ips.IsValidMask(input) {
+					bits = input
+					break
+				}
+				error("Please specify a valid argument!")
+			}
+		}
+
+		if choice == "1" {
+			bits = ips.ConvertMaskToBits(ips.ConvertToBinary(mask))
+		} else {
+			mask = ips.ConvertToDecimal(ips.CalculateMask(bits))
+		}
+
+		resTitle()
+		printInfo()
+
+		message("$", "If you want to exit, type 'yes', else type 'no'!")
+		prompt("choose")
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSuffix(input, "\n")
+		fmt.Print("\u001b[0m")
+
+		if strings.HasPrefix(input, "y") {
+			break
 		}
 	}
-
-	if choice == "1" {
-		bits = ips.ConvertMaskToBits(ips.ConvertToBinary(mask))
-	} else {
-		mask = ips.ConvertToDecimal(ips.CalculateMask(bits))
-	}
-
-	resTitle()
-	printInfo()
 }
 
 func printInfo() {
@@ -131,6 +143,8 @@ func printInfo() {
 
 	fmt.Print("\u001b[34;1m$ Max IP:            ")
 	fmt.Printf("\u001b[0m%s\n", ips.SubtractIps(ips.ConvertToDecimal(ips.BroadcastAddress(bits, ips.ConvertToBinary(ip))), "0.0.0.1"))
+
+	fmt.Println()
 }
 
 func message(prefix string, m string) {
